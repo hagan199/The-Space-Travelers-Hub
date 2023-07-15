d88902ed2328a2edbfcbe98091ff46657e1fd559
hagan23 <haganemmanuel23@gmail.com>
2023-07-15 18:03:14 +0000

    update on the test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import the BrowserRouter component
 import App from '../App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Router> {/* Wrap the component with the Router */}
+      <App />
+    </Router>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
